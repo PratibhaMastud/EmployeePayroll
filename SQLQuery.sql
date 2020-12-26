e11a9077b1620117acadcd883a92fed0d7d84cef
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-27 00:48:28 +0530

    [Pratibha Mastud] Add : Add gender in table

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -2,4 +2,5 @@ alter table [dbo].[employee_payroll] add gender char(1)
 
 update [dbo].[employee_payroll] set gender = 'M' where name = 'john' or name = 'bob'
 
-update [dbo].[employee_payroll] set gender = 'F' where name = 'charli
+update [dbo].[employee_payroll] set gender = 'F' where name = 'charlie' 
+ 
