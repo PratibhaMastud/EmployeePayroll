b5d3bf3e1d67167e389a71456946f734b685b67e
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-28 22:49:34 +0530

    [Pratibha Mastud] Add : Insert Values in table

---
+++ InsertData.sql
@@ -0,0 +1,5 @@
+insert into [dbo].[employee_payroll] values
+(101, 'john', 100000.00, '2002-04-03'),
+(102, 'bob', 200000.00, '2010-12-22'),
+(103, 'charlie', 300000.00, '2019-12-19')
+select * from employee_payroll
--- SQLQuery.sql
+++
@@ -1,6 +0,0 @@
-alter table [dbo].[employee_payroll] add gender char(1)
-
-update [dbo].[employee_payroll] set gender = 'M' where name = 'john' or name = 'bob'
-
-update [dbo].[employee_payroll] set gender = 'F' where name = 'charlie' 
- 
