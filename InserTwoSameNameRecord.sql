7c064dbfc65f1a9d5e155077c681e4a28850aef8
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-29 00:08:35 +0530

    [Pratibha Mastud] Add : Add one Employee in two Department

---
+++ InserTwoSameNameRecord.sql
@@ -0,0 +1,4 @@
+insert into employee_payroll values
+(105,'Terisa', 400000.00, '2020-10-2', 'M', 'Pune', '9833443083', 'Sales', 20000.00, 10000.00, 200000.00, 100000.00)
+
+select * from [dbo].[employee_payroll]
